5ae3287380ac8aad1d61cc37469cf6d49c0aedaa
Long <longhui.yang@inveno.cn>
2018-06-08 18:34:23 +0800

    未完成加密算法

--- TestJava/src/SecurityBuilder2.java
+++ TestJava/src/SecurityBuilder2.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -6,19 +5,19 @@ import java.util.Random;
  * @author long
  *
  */
-public final class SecurityBuilder {
+public final class SecurityBuilder2 {
 	
 	private static final String SECRET = "浪淘沙·李煜\r帘外雨潺潺，春意阑珊，罗衾不耐五更寒。梦里不知身是客，一晌贪欢。\r独自莫凭栏，无限江山，别时容易见时难。流水落花春去也，天上人间。";
-	
-	private static final void generate(String key){
+
+	private static final void generate2(String key){
 		
 		Random mRandom = new Random();
 		mRandom.setSeed(key.hashCode());
 		
 		int count = 0;
 		byte[] flag = new byte[256];//标记
-		HashMap<Byte,Byte> send_mapping = new HashMap<Byte,Byte>(256);
-		HashMap<Byte,Byte> recv_mapping = new HashMap<Byte,Byte>(256);
+		byte[] send_mapping = new byte[256];
+		byte[] recv_mapping = new byte[256];
 
 		byte min = Byte.MIN_VALUE;
 		byte max = Byte.MAX_VALUE;
@@ -37,43 +36,33 @@ public final class SecurityBuilder {
 			}
 			
 			//2.产生映射唤醒
-			send_mapping.put(i, value);
-			recv_mapping.put(value, i);
+			send_mapping[i + 128]= value;
+			recv_mapping[value + 128]= i;
 			
 			count++;
 			if(count >= 256){
 				break;
 			}
 		}
-		
-		System.out.println();
-		for(byte i = Byte.MIN_VALUE;i<= Byte.MAX_VALUE;i++){
-			if(i%16 == 0){
-				System.out.println();
-			}
-//			System.out.println("send "+ i +" = " + send_mapping.get(i));
-			System.out.print(send_mapping.get(i) + ",");
-			if(i==Byte.MAX_VALUE){
-				break;
-			}
-		}
-		
-		System.out.println();
-		for(byte i = Byte.MIN_VALUE;i<= Byte.MAX_VALUE;i++){
-			if(i%16 == 0){
-				System.out.println();
-			}
-//			System.out.println("send "+ i +" = " + recv_mapping.get(i));
-			System.out.print(recv_mapping.get(i) + ",");
-			if(i==Byte.MAX_VALUE){
-				break;
-			}
-		}
+	
+		print(send_mapping);
+		print(recv_mapping);
 	}
 
+	private static void print(byte[] data){
+		System.out.print("\r\n");
+		for(int i = 0;i<data.length;i++){
+			if(i%16 == 0){
+				System.out.println();
+			}
+			System.out.print(data[i]+",");
+		}
+		System.out.print("\r\n");
+	}
+	
 	//---------------------------------------------------
 	public static void main(String[] args) {
-		generate(SECRET);
+		generate2(SECRET);
 	}
 
 }
